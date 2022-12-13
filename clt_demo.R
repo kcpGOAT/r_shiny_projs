06088d31d3ed29bdafc1aaebbda2eba5e4718d9e
kcpGOAT <90441353+kcpGOAT@users.noreply.github.com>
2022-12-12 19:15:01 -0800

    Create clt_demo.R

---
+++ clt_demo.R
@@ -0,0 +1,97 @@
+library(shiny)
+
+ui <- fluidPage(
+  titlePanel("Demonstration of the Central Limit Theorem"),
+  sidebarLayout(
+    sidebarPanel(
+      selectInput("dist", "Distribution", c("Normal", "Uniform", "Poisson", "Binomial")), 
+      numericInput("n_sample", "Number of samples", value = 50), 
+      numericInput("size", "Sample size", value = 100),
+      sliderInput("n_bins", "Number of bins", min = 5, max = 50, value = 10)
+    ),
+    mainPanel(
+      tabsetPanel(
+        tabPanel("Sample means", plotOutput("hist_mean")),
+        tabPanel("Sample sums", plotOutput("hist_sum")),
+        tabPanel("Population", plotOutput("hist_pop"))
+      )
+    )
+  ),
+  tabsetPanel(
+    id = "params",
+    type = "hidden",
+    tabPanel("Normal",
+             numericInput("mean", "Mean", value = 0),
+             numericInput("sd", "SD", value = 1)
+    ),
+    tabPanel("Uniform",
+             numericInput("min", "Min", value = 0),
+             numericInput("max", "Max", value = 1)
+    ),
+    tabPanel("Poisson",
+             numericInput("r", "Rate", value = 1)
+    ),
+    tabPanel("Binomial",
+             numericInput("p", "Probability of success", value = 0.5),
+             numericInput("n", "Number of trials", value = 10)
+    )
+  ),
+)
+
+server <- function(input, output, session) {
+  observeEvent(input$dist, {
+    updateTabsetPanel(inputId = "params", selected = input$dist)
+  })
+  
+  samples <- reactive({
+    switch(input$dist, 
+      Normal = replicate(n = input$n_sample, 
+                         rnorm(input$size, input$mean, input$sd)),
+      Uniform = replicate(n = input$n_sample, 
+                          runif(input$size, input$min, input$max)), 
+      Poisson = replicate(n = input$n_sample, 
+                          rpois(input$size, input$r)), 
+      Binomial = replicate(n = input$n_sample, 
+                           rbinom(input$size, input$n, input$p)))
+  })
+  sample_dist_mean <- reactive({
+      apply(samples(), MARGIN = 2, mean) |>
+        unlist() |> 
+        as.numeric()
+  })
+  sample_dist_sum <- reactive({
+    apply(samples(), MARGIN = 2, sum) |>
+      unlist() |>
+      as.numeric()
+  })
+  pop_dist <- reactive({
+    as.vector(samples())
+  })
+  
+  output$hist_mean <- renderPlot({
+      hist(sample_dist_mean(), 
+           main = "Sampling distribution of the mean",
+           xlab = "Sample means",
+           col = "blue", 
+           breaks = input$n_bins, 
+           density = 50)
+    })
+  output$hist_sum <- renderPlot({
+      hist(sample_dist_sum(), 
+           main = "Sampling distribution of the sum",
+           xlab = "Sample sums", 
+           col = "red",
+           breaks = input$n_bins, 
+           density = 50)
+    })
+  output$hist_pop <- renderPlot({
+    hist(pop_dist(), 
+         main = "Overall population distribution", 
+         xlab = "x", 
+         col = "purple", 
+         breaks = input$n_bins, 
+         density = 50)
+  })
+}
+
+shinyApp(ui, server)
