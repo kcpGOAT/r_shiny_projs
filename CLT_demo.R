84938cf014deb8a5e888ac3f3c5fab53bee893d9
kcpGOAT <90441353+kcpGOAT@users.noreply.github.com>
2022-12-13 22:31:43 -0800

    Update CLT_demo.R

--- CLT_demo.R
+++ CLT_demo.R
@@ -4,19 +4,34 @@ ui <- fluidPage(
   titlePanel("Demonstration of the Central Limit Theorem"),
   sidebarLayout(
     sidebarPanel(
-      selectInput("dist", "Distribution", c("Normal", "Uniform", "Poisson", "Binomial")), 
+      radioButtons("dist", "Distribution", c("Normal", "Uniform", "Poisson", "Binomial", "Beta"), 
+                   selected = "Normal"), 
       numericInput("n_sample", "Number of samples", value = 50), 
       numericInput("size", "Sample size", value = 100),
-      sliderInput("n_bins", "Number of bins", min = 5, max = 50, value = 10)
+      sliderInput("n_bins", "Number of bins", min = 5, max = 50, value = 10),
+      checkboxInput("prob", "Set y-axis to density", value = FALSE)
     ),
     mainPanel(
       tabsetPanel(
+        id = "stats",
         tabPanel("Sample means", plotOutput("hist_mean")),
         tabPanel("Sample sums", plotOutput("hist_sum")),
         tabPanel("Population", plotOutput("hist_pop"))
+      ),
+      tabsetPanel(
+        id = "summary_stats", 
+        type = "hidden", 
+        tabPanel("Sample means", 
+                 verbatimTextOutput("summary_mean")),
+        tabPanel("Sample sums",
+                 verbatimTextOutput("summary_sum")),
+        tabPanel("Population",
+                 verbatimTextOutput("summary_pop"))
       )
     )
   ),
+  h1("Parameters", style = "font-size:20px;"),
+  
   tabsetPanel(
     id = "params",
     type = "hidden",
@@ -34,13 +49,20 @@ ui <- fluidPage(
     tabPanel("Binomial",
              numericInput("p", "Probability of success", value = 0.5),
              numericInput("n", "Number of trials", value = 10)
+    ),
+    tabPanel("Beta", 
+             numericInput("a", "α", value = 5),
+             numericInput("b", "β", value = 5)
     )
-  ),
+  )
 )
 
 server <- function(input, output, session) {
   observeEvent(input$dist, {
     updateTabsetPanel(inputId = "params", selected = input$dist)
+  })
+  observeEvent(input$stats, {
+    updateTabsetPanel(inputId = "summary_stats", selected = input$stats)
   })
   
   samples <- reactive({
@@ -52,7 +74,10 @@ server <- function(input, output, session) {
       Poisson = replicate(n = input$n_sample, 
                           rpois(input$size, input$r)), 
       Binomial = replicate(n = input$n_sample, 
-                           rbinom(input$size, input$n, input$p)))
+                           rbinom(input$size, input$n, input$p)),
+      Beta = replicate(n = input$n_sample, 
+                       rbeta(input$size, input$a, input$b))
+    )
   })
   sample_dist_mean <- reactive({
       apply(samples(), MARGIN = 2, mean) |>
@@ -69,7 +94,8 @@ server <- function(input, output, session) {
   })
   
   output$hist_mean <- renderPlot({
-      hist(sample_dist_mean(), 
+      hist(sample_dist_mean(),
+           prob = input$prob,
            main = "Sampling distribution of the mean",
            xlab = "Sample means",
            col = "blue", 
@@ -77,7 +103,8 @@ server <- function(input, output, session) {
            density = 50)
     })
   output$hist_sum <- renderPlot({
-      hist(sample_dist_sum(), 
+      hist(sample_dist_sum(),
+           prob = input$prob,
            main = "Sampling distribution of the sum",
            xlab = "Sample sums", 
            col = "red",
@@ -85,12 +112,23 @@ server <- function(input, output, session) {
            density = 50)
     })
   output$hist_pop <- renderPlot({
-    hist(pop_dist(), 
+    hist(pop_dist(),
+         prob = input$prob,
          main = "Overall population distribution", 
          xlab = "x", 
          col = "purple", 
          breaks = input$n_bins, 
          density = 50)
+  })
+
+  output$summary_mean <- renderPrint({
+    summary(sample_dist_mean())
+  })
+  output$summary_sum <- renderPrint({
+    summary(sample_dist_sum())
+  })
+  output$summary_pop <- renderPrint({
+    summary(pop_dist())
   })
 }
 
